13b195c8dcdd044488437b616845a6e969b7e440
saad8554656 <ks136647@gmail.com>
2025-05-08 02:37:51 +0530

    second commit

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const   router = express.Router();
-const taskController = require('../controllers/taskcontroller.js');
+const taskController = require('../controllers/taskcontroller');
 
 router.get("/", taskController.getTasks);
 router.post("/", taskController.createTask);
